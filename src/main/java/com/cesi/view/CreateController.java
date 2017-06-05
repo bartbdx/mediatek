cbef93f15ac77d5c1cc9446195e953a6b07abd76
lav <pl_dtm@hotmail.com>
2017-06-05 08:50:37 +0200

    books and musique added

--- src/main/java/com/cesi/view/CreateController.java
+++ src/main/java/com/cesi/view/CreateController.java
@@ -2,10 +2,13 @@ package com.cesi.view;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -34,6 +37,27 @@ public class CreateController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        livreBtn.setOnMouseClicked((MouseEvent event) -> {
+            try {
+                handleButton("/books.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        musiqueBtn.setOnMouseClicked((MouseEvent event) -> {
+            try {
+                handleButton("/musique.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
+    @FXML
+    public void handleButton(String panes) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(panes));
+        Pane fillPane = (Pane) fxmlLoader.load();
+        downPane.getChildren().clear();
+        downPane.getChildren().add(fillPane);
     }
 }
